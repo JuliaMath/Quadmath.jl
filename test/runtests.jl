d20c4f84002296e3df40ee8bd8594e9a2a693e7f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-02 00:51:03 -0500

    test BigInt, flipsign, modf (#14)

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,3 +21,33 @@ end
     @test Float64(x+y) == Float64(BigFloat(x) + BigFloat(y))
     @test x+y == Float128(BigFloat(x) + BigFloat(y))
 end
+
+@testset "BigInt" begin
+    x = parse(Float128, "100.0")
+    y = parse(Float128, "25.0")
+    @test Float64(x+y) == Float64(BigInt(x) + BigInt(y))
+    @test x+y == Float128(BigInt(x) + BigInt(y))    
+end
+
+@testset "flipsign" begin
+    x = Float128( 2.0)
+    y = Float128(-2.0)
+    @test x == flipsign(y, -one(Float128))
+    @test y == flipsign(y,  1)
+end
+
+@testset "modf" begin
+    x = Float128(pi)
+    fpart, ipart = modf(x)
+    @test x == ipart + fpart
+    @test signbit(fpart) == signbit(ipart) == false
+
+    y = Float128(-pi)
+    fpart, ipart = modf(y)
+    @test y == ipart + fpart
+    @test signbit(fpart) == signbit(ipart) == true
+    
+    z = x^3
+    fpart, ipart = modf(x) .+ modf(y)
+    @test x+y == ipart+fpart
+end
