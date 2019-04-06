6c9a8a309a46b69602abb9e64c8f95353ec8881f
Ralph A. Smith <smith.ralph.a@gmail.com>
2019-04-06 13:27:50 -0400

    accommodate < v1.1 digits usage

--- src/printf.jl
+++ src/printf.jl
@@ -1,11 +1,21 @@
 import Base.Printf: ini_dec, fix_dec, ini_hex, ini_HEX
 
+if VERSION < v"1.1"
+using Base.Grisu: DIGITSs
+fix_dec(out, d::Float128, flags::String, width::Int, precision::Int, c::Char) = fp128_printf(out, d, flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_dec(out, d::Float128, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) = fp128_printf(out, d, flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_hex(out, d::Float128, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) = fp128_printf(out, d, flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_HEX(out, d::Float128, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) = fp128_printf(out, d, flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_hex(out, d::Float128, flags::String, width::Int, precision::Int, c::Char) = fp128_printf(out, d, flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_HEX(out, d::Float128, flags::String, width::Int, precision::Int, c::Char) = fp128_printf(out, d, flags, width, precision, c, DIGITSs[Threads.threadid()])
+else
 fix_dec(out, d::Float128, flags::String, width::Int, precision::Int, c::Char, digits) = fp128_printf(out, d, flags, width, precision, c, digits)
 ini_dec(out, d::Float128, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) = fp128_printf(out, d, flags, width, precision, c, digits)
 ini_hex(out, d::Float128, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) = fp128_printf(out, d, flags, width, precision, c, digits)
 ini_HEX(out, d::Float128, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) = fp128_printf(out, d, flags, width, precision, c, digits)
 ini_hex(out, d::Float128, flags::String, width::Int, precision::Int, c::Char, digits) = fp128_printf(out, d, flags, width, precision, c, digits)
 ini_HEX(out, d::Float128, flags::String, width::Int, precision::Int, c::Char, digits) = fp128_printf(out, d, flags, width, precision, c, digits)
+end
 
 function fp128_printf(out, d::Float128, flags::String, width::Int, precision::Int, c::Char, digits)
     fmt_len = sizeof(flags)+4
@@ -35,9 +45,6 @@ function fp128_printf(out, d::Float128, flags::String, width::Int, precision::In
     @assert length(printf_fmt) == fmt_len
     bufsiz = length(digits)
     lng = @ccall(libquadmath.quadmath_snprintf(digits::Ptr{UInt8}, bufsiz::Csize_t, printf_fmt::Ptr{UInt8}, d::(Cfloat128...))::Cint)
-#    lng = ccall((:mpfr_snprintf,:libmpfr), Int32,
-#                (Ptr{UInt8}, Culong, Ptr{UInt8}, Ref{Float}...),
-#                digits, bufsiz, printf_fmt, d)
     lng > 0 || error("invalid printf formatting for Float128")
     unsafe_write(out, pointer(digits), min(lng, bufsiz-1))
     return (false, ())
