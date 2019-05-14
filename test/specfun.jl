7a21f81bd56341807fae81aa06a0269f3105470b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-14 04:37:21 -0400

    Update specfun.jl

--- test/specfun.jl
+++ test/specfun.jl
@@ -1,13 +1,17 @@
+import SpecialFunctions
+
 @testset "special functions" begin
     # The intention here is not to check the accuracy of the libraries,
     # rather the sanity of library calls:
     piq = Float128(pi)
     halfq = Float128(0.5)
-    @test gamma(halfq) ≈ sqrt(piq)
-    for func in (erf, erfc, besselj0, besselj1, bessely0, bessely1, lgamma)
+    @test SpecialFunctions.gamma(halfq) ≈ sqrt(piq)
+    for func in (SpecialFunctions.erf, SpecialFunctions.erfc, 
+                 SpecialFunctions.besselj0, SpecialFunctions.besselj1, 
+                 SpecialFunctions.bessely0, SpecialFunctions.bessely1, SpecialFunctions.lgamma)
         @test func(halfq) ≈ func(0.5)
     end
-    for func in (bessely, besselj)
+    for func in (SpecialFunctions.bessely, SpecialFunctions.besselj)
         @test func(3,halfq) ≈ func(3,0.5)
     end
 end
