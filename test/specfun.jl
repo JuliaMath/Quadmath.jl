94e1f450ae317ab3b7969d406a4523522100bc98
Mateus Araújo <maltusan@gmail.com>
2024-04-05 03:53:43 +0200

    add logabsgamma (#72)
    * add logabsgamma
    
    * Update src/specfun.jl
    
    Co-authored-by: RalphAS <ralphas@users.noreply.github.com>
    
    * handle Inf and NaN
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    * match sign also for negative integers
    
    * remove deprecated lgamma (SpecialFunctions will take care of it)
    
    ---------
    
    Co-authored-by: RalphAS <ralphas@users.noreply.github.com>
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- test/specfun.jl
+++ test/specfun.jl
@@ -6,10 +6,13 @@ using SpecialFunctions
     piq = Float128(pi)
     halfq = Float128(0.5)
     @test gamma(halfq) ≈ sqrt(piq)
-    for func in (erf, erfc, besselj0, besselj1, bessely0, bessely1, lgamma)
+    for func in (erf, erfc, besselj0, besselj1, bessely0, bessely1, loggamma)
         @test func(halfq) ≈ func(0.5)
     end
     for func in (bessely, besselj)
         @test func(3,halfq) ≈ func(3,0.5)
     end
+    @test gamma(Float128(2),3) ≈ gamma(2,3)
+    @test all(logabsgamma(Float128(-0.5)) .≈ logabsgamma(-0.5))
+    @test all(logabsgamma(Float128(-1.5)) .≈ logabsgamma(-1.5))
 end
