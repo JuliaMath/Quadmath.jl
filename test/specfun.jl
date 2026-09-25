56b25f1eb8b83aea7dd6bb24cf545ea1c288def7
Alex Arslan <ararslan@comcast.net>
2026-09-25 09:24:15 -0700

    Add AArch64 support by using `long double` with libm
    Unlike x86-64, on AArch64, `long double` is IEEE binary128 and there is
    no libquadmath. We can support that architecture in this package by
    conditionally calling `long double` functions from libm. The only
    difference in functionality is that the Bessel functions do not have
    `long double` equivalents.
    
    With these changes, all tests pass on FreeBSD AArch64.

--- test/specfun.jl
+++ test/specfun.jl
@@ -1,4 +1,5 @@
 using SpecialFunctions
+using Quadmath: USE_LONG_DOUBLE
 
 @testset "special functions" begin
     # The intention here is not to check the accuracy of the libraries,
@@ -6,11 +7,17 @@ using SpecialFunctions
     piq = Float128(pi)
     halfq = Float128(0.5)
     @test gamma(halfq) ≈ sqrt(piq)
-    for func in (erf, erfc, besselj0, besselj1, bessely0, bessely1, loggamma)
+    for func in (erf, erfc, loggamma)
         @test func(halfq) ≈ func(0.5)
     end
-    for func in (bessely, besselj)
-        @test func(3,halfq) ≈ func(3,0.5)
+    if !USE_LONG_DOUBLE
+        # There is no (common) long double implementation of Bessel functions
+        for func in (besselj0, besselj1, bessely0, bessely1)
+            @test func(halfq) ≈ func(0.5)
+        end
+        for func in (bessely, besselj)
+            @test func(3,halfq) ≈ func(3,0.5)
+        end
     end
     @test gamma(Float128(2),3) ≈ gamma(2,3)
     @test all(logabsgamma(Float128(-0.5)) .≈ logabsgamma(-0.5))
